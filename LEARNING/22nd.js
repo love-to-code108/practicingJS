8fa0928045f9541698284244edbd90d74b7ee704
love-to-code108 <love.to.code108@gmail.com>
2023-10-22 10:55:41 +0530

    concept of console.table() has been added

--- LEARNING/22nd.js
+++ LEARNING/22nd.js
@@ -1,4 +1,13 @@
-console.table({"99":1344,"hello world":12,"55":69});
 
 // console.table() can take both arrays and objects ;
-// console.table() will always follow an ascending order for the elements in the index part of the 
+// console.table() creates two columbs namely (index) and values
+// console.table() will always follow an ascending order for the elements in the index part that is for the keys they will be ordered in ascending order , 
+// if its an array it will be in the order of its index number , for eg;
+
+// array example
+console.table(["h","e","l","l","o"]);
+
+// object example
+console.table({"99":1344,"hello world":12,"55":69});
+
+// after you compile the program as you can see 55 comes first and then 99 because they are in ascending order
