a5bebaf8fa36d86c47c09179f42669cdcd5f11dc
love-to-code108 <love.to.code108@gmail.com>
2023-10-22 10:49:30 +0530

    adding all the chai with code js teachings

---
+++ LEARNING/22nd.js
@@ -0,0 +1,4 @@
+console.table({"99":1344,"hello world":12,"55":69});
+
+// console.table() can take both arrays and objects ;
+// console.table() will always follow an ascending order for the elements in the index part of the 
